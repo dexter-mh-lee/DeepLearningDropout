4ec73edc434c4e968a4e2b6b8a5825b1d1527732
jamesduyck <jamesduyck@gmail.com>
2014-10-20 15:41:58 -0400

    Faster
    Dexter improved speed.

--- maxPooling.m
+++ maxPooling.m
@@ -1,7 +1,7 @@
-function [maxValue, indexMatrix] = maxPooling(matrix)
+function [maxValue] = maxPooling(matrix)
 	% maxValue - vector of maximum values
 	% indexMatrix - 3d matrix with same dimensions as the input, 1 if maximum 0 if not
-	maxValue = squeeze(max(max(matrix)));
-	indexMatrix = arrayfun(@(i) matrix(:,:,i) == maxValue(i), 1:size(matrix,3), 'UniformOutput', false);
-	indexMatrix = cat(3, indexMatrix{:});
+	maxValue = max(max(matrix));
+	%indexMatrix = arrayfun(@(i) matrix(:,:,i) == maxValue(i), 1:size(matrix,3), 'UniformOutput', false);
+	%indexMatrix = cat(3, indexMatrix{:});
 end
--- maxPoolingLayer.m
+++ maxPoolingLayer.m
@@ -1,12 +1,20 @@
 function [z, k] = maxPoolingLayer(a, mapSize, scale)
 	%This may be vectorized further please check
-	z = zeros([mapSize(1:2), size(a,3)]); % holder for the result matrix
-	k = zeros(size(a)); % holder for the transition matrix
-	for i = 1:mapSize(1)
-		for j = 1:mapSize(2)
-			% Divide the whole matrix into patches and apply maxPooling
-			[z(i,j,:), k(scale * (i - 1) + 1:scale * i, scale * (j - 1) + 1:scale * j, :)] ...
-				= maxPooling(a(scale * (i - 1) + 1:scale * i, scale * (j - 1) + 1:scale * j, :));
-		end
-	end
+% 	z = zeros([mapSize(1:2), size(a,3)]); % holder for the result matrix
+% 	k = zeros(size(a)); % holder for the transition matrix
+% 	for i = 1:mapSize(1)
+% 		for j = 1:mapSize(2)
+% 			% Divide the whole matrix into patches and apply maxPooling
+% 			[z(i,j,:)] ...
+% 				= maxPooling(a(scale * (i - 1) + 1:scale * i, scale * (j - 1) + 1:scale * j, :));
+% 		end
+%     end
+    z = arrayfun(@(i) ...
+            arrayfun(@(j) ...
+                maxPooling(a(scale * (i - 1) + 1:scale * i, scale * (j - 1) + 1:scale * j, :)), ...
+                1:mapSize(2), 'UniformOutput', false), ...
+            1:mapSize(1), 'UniformOutput', false);
+    z = cell2mat(cellfun(@(maxArray) transpose3d(cell2mat(maxArray)), z, 'UniformOutput', false));
+    z = transpose3d(z);
+    k = expand(z, [scale, scale, 1]) == a;
 end
