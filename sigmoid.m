694cab44d73f1b3d1efaad097fd74c770fd897fa
mhlee1116 <mhlee1116@gmail.com>
2014-10-28 21:53:09 -0400

    noremal nn version added

--- sigmoid.m
+++
@@ -1,5 +0,0 @@
-function result = sigmoid(v)
-
-  result = 1./(1+exp(-1*v));
-
-end
--- transpose3d.m
+++
@@ -1,4 +0,0 @@
-function result = transpose3d(mat)
-	result = arrayfun(@(k) mat(:,:,k)', 1:size(mat, 3), 'UniformOutput', false);
-	result = cat(3,result{:});
-end
