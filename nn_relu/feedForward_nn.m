d6d7c5632e9f286b60d2e9337bde3cfde8539a05
mhlee1116 <mhlee1116@gmail.com>
2014-11-27 00:06:26 -0500

    relu update

--- nn_relu/feedForward_nn.m
+++ nn_relu/feedForward_nn.m
@@ -1,24 +1,21 @@
 function net = feedForward_nn(net, x, opt, epochNum)
 
-	numLayers = length(net.layers); % total number of layers
-	net.layers{1}.a = x;
+  numLayers = length(net.layers); % total number of layers
+  net.layers{1}.a = x;
   ido = opt.input_do_rate(epochNum);
   hdo = opt.hidden_do_rate(epochNum);
-  dao = opt.do_again_rate;
-  isFirst = false;
-  if net.layers{1}.do(1, 1) < 0
-      isFirst = true;
-  end
   if opt.gaussian
       noiseRate = 1-opt.noiseScale*(1-ido); %Scale noise from dropout rate.
       noiseSD = sqrt((1-noiseRate)/noiseRate); %Choose variance, then find standard deviation
       net.layers{1}.ga = normrnd(1, noiseSD, size(net.layers{1}.a));
       net.layers{1}.a = net.layers{1}.a .* net.layers{1}.ga;
   end
-  net.layers{1}.dc = ones(size(net.layers{2}.w));
   if opt.dropout
-      if opt.adaptive && isFirst == false
-          threshold = 1 - net.layers{1}.do * (1 - 2 * dao) - dao;
+      if opt.adaptive
+          %We want 0s changed to 1s with probability 1 (dropped out
+          %probability 0) and 1s changed to 0s with probability (1-ido)/ido
+          threshold = 1 - net.layers{1}.do * (1-ido)/ido;
+          %threshold = (1+ido)/2 - net.layers{1}.do * ((1+ido)/2 - 1 +((1-ido)/ido)*(1+ido)/2);
           net.layers{1}.do = rand(size(net.layers{1}.a)) <= threshold;
       else
           net.layers{1}.do = rand(size(net.layers{1}.a)) <= ido;
@@ -26,11 +23,14 @@ function net = feedForward_nn(net, x, opt, epochNum)
       net.layers{1}.a = net.layers{1}.a .* net.layers{1}.do;
   elseif opt.dropconnect
       net.layers{1}.dc = rand(size(net.layers{2}.w)) <= ido;
+      net.layers{2}.wdc = net.layers{2}.w .* net.layers{1}.dc;
   end
-  net.layers{2}.wdc = net.layers{2}.w .* net.layers{1}.dc;
-
-	for l = 2 : numLayers
-		net.layers{l}.a = bsxfun(@plus, net.layers{l}.wdc * net.layers{l - 1}.a, net.layers{l}.b);
+  for l = 2 : numLayers
+    if opt.dropconnect
+      net.layers{l}.a = bsxfun(@plus, net.layers{l}.wdc * net.layers{l - 1}.a, net.layers{l}.b);
+    else
+      net.layers{l}.a = bsxfun(@plus, net.layers{l}.w * net.layers{l - 1}.a, net.layers{l}.b);
+    end
     net.layers{l}.a(net.layers{l}.a < 0) = 0;
     if l < numLayers && opt.gaussian
         noiseRate = 1-opt.noiseScale*(1-hdo);
@@ -39,10 +39,11 @@ function net = feedForward_nn(net, x, opt, epochNum)
         net.layers{l}.a = net.layers{l}.a .* net.layers{l}.ga;
     end
     if l < numLayers
-      net.layers{l}.dc = ones(size(net.layers{l+1}.w));
       if opt.dropout
-        if opt.adaptive && isFirst == false
-            threshold = 1 - net.layers{l}.do * (1 - 2 * dao) - dao;
+        if opt.adaptive
+            %As before, but with hdo
+            threshold = 1 - net.layers{l}.do * (1-hdo)/hdo;
+            %threshold = (1+hdo)/2 - net.layers{l}.do * ((1+hdo)/2 - 1 +((1-hdo)/hdo)*(1+hdo)/2);
             net.layers{l}.do = rand(size(net.layers{l}.a)) <= threshold;
         else
             net.layers{l}.do = rand(size(net.layers{l}.a)) <= hdo;
@@ -50,8 +51,8 @@ function net = feedForward_nn(net, x, opt, epochNum)
         net.layers{l}.a = net.layers{l}.a .* net.layers{l}.do;
       elseif opt.dropconnect
         net.layers{l}.dc = rand(size(net.layers{l+1}.w)) <= hdo;
+        net.layers{l+1}.wdc = net.layers{l+1}.w .* net.layers{l}.dc;
       end
-      net.layers{l+1}.wdc = net.layers{l+1}.w .* net.layers{l}.dc;
     end
-	end
+  end
 end
